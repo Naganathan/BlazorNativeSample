bc92b0a064595d2f479ccd7143a8a3a01b77be0a
naganthanganeshbabu <naganathanganeshbabu@syncfusion.com>
2020-10-15 18:41:50 +0530

    Samples committed

---
+++ test.js
@@ -0,0 +1,21 @@
+﻿window.getDiagramSize = function () {
+    var element = document.getElementById("canvas");
+    var bounds = element.getBoundingClientRect();
+    var size = {};
+    size["X"] = bounds.left;
+    size["Y"] = bounds.top;
+    return size;
+}
+
+window.getScrollSize = function (element) {
+    var scrollSize = {};
+    scrollSize["X"] = element.scrollLeft;
+    scrollSize["Y"] = element.scrollTop;
+    return scrollSize;
+}
+
+window.setPanScroll = function (element, left, top) {
+    element.scrollLeft += left;
+    element.scrollTop += top;
+    return { "X": element.scrollLeft, "Y": element.scrollTop };
+}
